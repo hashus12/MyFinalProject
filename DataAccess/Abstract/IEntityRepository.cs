29fc433c14b52290e377488fdd398d20fd1a2851
has ozg <hasan_ozgen@windowslive.com>
2021-02-04 00:02:47 +0300

    31 ocak 2021

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint - generic kısıt
+    //class : referans tip olabilir.
+    //IEntity: IEntity olabilir veya IEntity implemente eden bir nesne olabilir.
+    //new() : new'lenebilir olmalı
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        //Generic repository design pattern
+        //Linq Expression
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        //Refactoring
+        T Get(Expression<Func<T, bool>> filter = null);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
